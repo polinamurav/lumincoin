3c31072ef906a890538de4d7f14769b4e400e543
Polina Muravieva <polinamur3@gmail.com>
2024-10-27 23:42:18 +0300

    layout main page

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -1,25 +1,30 @@
 import { Chart, registerables } from 'chart.js';
+import {IncomeExpensesService} from "./services/income&expenses-service";
 
 export class Main {
     constructor() {
-        console.log('ГЛАВНАЯ');
-
         Chart.register(...registerables);
 
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const ctx2 = document.getElementById('diagram2').getContext('2d');
+        this.buttonFilters();
+        this.activeButton('today');
+        this.initializeChart();
+    }
 
-        const data = {
-            labels: [
-                'Red',
-                'Orange',
-                'Yellow',
-                'Green',
-                'Blue'
-            ],
+    async initializeChart() {
+        const result = await this.getIncomeExpenses('all');
+
+        // фильтр на доходы и расходы
+        const incomeData = result.filter(item => item.type === 'income');
+        const expenseData = result.filter(item => item.type === 'expense');
+
+        const incomeLabels = incomeData.map(item => item.category);
+        const incomeValues = incomeData.map(item => item.amount);
+
+        const incomeChartData = {
+            labels: incomeLabels,
             datasets: [{
-                label: 'My First Dataset',
-                data: [250, 50, 100, 80, 60],
+                label: 'Доходы',
+                data: incomeValues,
                 backgroundColor: [
                     '#DC3545',
                     '#FD7E14',
@@ -31,11 +36,12 @@ export class Main {
             }]
         };
 
+        const ctx = document.getElementById('myChart').getContext('2d');
         new Chart(ctx, {
             type: 'pie',
-            data: data,
+            data: incomeChartData,
             options: {
-                responsive: true, //адаптивность графика
+                responsive: true,
                 plugins: {
                     legend: {
                         position: 'top',
@@ -44,11 +50,32 @@ export class Main {
             }
         });
 
+
+        const expenseLabels = expenseData.map(item => item.category);
+        const expenseValues = expenseData.map(item => item.amount);
+
+        const expenseChartData = {
+            labels: expenseLabels,
+            datasets: [{
+                label: 'Расходы',
+                data: expenseValues,
+                backgroundColor: [
+                    '#DC3545',
+                    '#FD7E14',
+                    '#FFC107',
+                    '#20C997',
+                    '#0D6EFD',
+                ],
+                hoverOffset: 4
+            }]
+        };
+
+        const ctx2 = document.getElementById('diagram2').getContext('2d');
         new Chart(ctx2, {
             type: 'pie',
-            data: data,
+            data: expenseChartData,
             options: {
-                responsive: true, //адаптивность графика
+                responsive: true,
                 plugins: {
                     legend: {
                         position: 'top',
@@ -56,5 +83,73 @@ export class Main {
                 }
             }
         });
+
     }
+
+    buttonFilters() {
+        document.querySelector('button[data-period="today"]').addEventListener('click', () => {
+            this.getIncomeExpenses('all');
+            this.activeButton('today');
+        });
+
+        document.querySelector('button[data-period="all"]').addEventListener('click', () => {
+            this.getIncomeExpenses('all');
+            this.activeButton('all');
+        });
+
+        document.querySelector('button[data-period="week"]').addEventListener('click', () => {
+            const today = new Date();
+            const lastWeek = new Date();
+            lastWeek.setDate(today.getDate() - 7);
+            this.getIncomeExpenses('interval', lastWeek.toISOString().split('T')[0], today.toISOString().split('T')[0]);
+            this.activeButton('week');
+        });
+        document.querySelector('button[data-period="month"]').addEventListener('click', () => {
+            const today = new Date();
+            const lastMonth = new Date();
+            lastMonth.setMonth(today.getMonth() - 1);
+            this.getIncomeExpenses('interval', lastMonth.toISOString().split('T')[0], today.toISOString().split('T')[0]);
+            this.activeButton('month');
+        });
+        document.querySelector('button[data-period="year"]').addEventListener('click', () => {
+            const today = new Date();
+            const lastYear = new Date();
+            lastYear.setFullYear(today.getFullYear() - 1);
+            this.getIncomeExpenses('interval', lastYear.toISOString().split('T')[0], today.toISOString().split('T')[0]);
+            this.activeButton('year');
+        });
+
+        const fromDateInput = document.querySelector('input[name="dateFrom"]');
+        const toDateInput = document.querySelector('input[name="dateTo"]');
+        document.querySelector('button[data-period="interval"]').addEventListener('click', () => {
+            const dateFrom = fromDateInput.value || 'null';
+            const dateTo = toDateInput.value || 'null';
+            this.getIncomeExpenses('interval', dateFrom, dateTo);
+            this.activeButton('interval');
+        });
+    }
+
+    activeButton(period) {
+        const buttons = document.querySelectorAll('button[data-period]');
+        buttons.forEach(button => {
+            button.classList.remove('btn-secondary');
+        })
+
+        const activeButton = document.querySelector(`button[data-period="${period}"]`);
+        if (activeButton) {
+            activeButton.classList.add('btn-secondary');
+        }
+    }
+
+    async getIncomeExpenses(period = 'all', dateFrom = 'null', dateTo = 'null') {
+        const response = await IncomeExpensesService.getIncomeExpenses(period, dateFrom, dateTo);
+
+        if (!response) {
+            alert('Произошла ошибка');
+            return window.location.href = '#/';
+        }
+
+        return response;
+    }
+
 }
