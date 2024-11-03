49ad3597b6f268d662fb189251d5e9652ae523f9
Polina Muravieva <polinamur3@gmail.com>
2024-11-03 16:34:10 +0300

    url-utils.js file converted to ts

--- frontend/src/components/services/url-utils.ts
+++ frontend/src/components/services/url-utils.ts
@@ -1,7 +1,8 @@
+//done
 export class UrlUtils {
-    static getUrlParam(param) {
-        const hash = window.location.hash;
-        const urlParams = new URLSearchParams(hash.split('?')[1]);
+    public static getUrlParam(param: string): string | null {
+        const hash: string = window.location.hash;
+        const urlParams: URLSearchParams = new URLSearchParams(hash.split('?')[1]);
         return urlParams.get(param);
     }
 }
