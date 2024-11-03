7ca586689e4a5d372e6b817357f0b1e19c76d472
Polina Muravieva <polinamur3@gmail.com>
2024-11-03 23:09:04 +0300

    expense-create.js file converted to ts

--- frontend/src/components/category/expense/expense-create.ts
+++ frontend/src/components/category/expense/expense-create.ts
@@ -1,39 +1,52 @@
 import {ExpenseService} from "../../services/expense-service";
+import {CategoryExpenseType} from "../../../types/category-expense.type";
 
+//done
 export class ExpenseCreate {
+    readonly titleInputElement: HTMLInputElement | null;
+
     constructor() {
-        document.getElementById('createButton').addEventListener('click', this.saveIncome.bind(this));
-
-        this.titleInputElement = document.getElementById('titleInput');
-
+        const createButton: HTMLElement | null = document.getElementById('createButton');
+        if (createButton) {
+            createButton.addEventListener('click', this.saveIncome.bind(this));
+        }
+        this.titleInputElement = document.getElementById('titleInput') as HTMLInputElement;
     }
 
-    validateForm() {
-        let isValid = true;
-        if (this.titleInputElement.value) {
-            this.titleInputElement.classList.remove('is-invalid');
-        } else {
-            this.titleInputElement.classList.add('is-invalid');
-            isValid = false;
+    private validateForm(): boolean {
+        let isValid: boolean = true;
+        if (this.titleInputElement) {
+            if (this.titleInputElement.value) {
+                this.titleInputElement.classList.remove('is-invalid');
+            } else {
+                this.titleInputElement.classList.add('is-invalid');
+                isValid = false;
+            }
         }
         return isValid;
     }
 
-    async saveIncome(e) {
+    private async saveIncome(e: MouseEvent): Promise<void> {
         e.preventDefault();
 
         if (this.validateForm()) {
-            const createData = {
+            if (!this.titleInputElement) {
+                window.location.href = '#/income';
+                return;
+            }
+            const createData: CategoryExpenseType = {
                 title: this.titleInputElement.value,
             };
 
-            const response = await ExpenseService.createExpense(createData);
+            const response: CategoryExpenseType = await ExpenseService.createExpense(createData);
 
             if (!response) {
                 alert("Произошла ошибка");
+                window.location.href = '#/income';
+                return;
             }
 
-            return window.location.href = '#/income';
+            window.location.href = '#/income';
         }
     }
 }
--- frontend/src/types/category-expense.type.ts
+++ frontend/src/types/category-expense.type.ts
@@ -1,4 +1,4 @@
 export type CategoryExpenseType = {
-    id: number;
+    id?: number;
     title: string;
 }
