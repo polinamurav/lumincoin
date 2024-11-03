85591eb174feab1d8f2b8707868e85e633c1b911
Polina Muravieva <polinamur3@gmail.com>
2024-11-03 23:52:59 +0300

    income-edit.js file converted to ts

--- frontend/src/components/category/income/income-edit.ts
+++ frontend/src/components/category/income/income-edit.ts
@@ -1,13 +1,22 @@
 import {IncomeService} from "../../services/income-service";
 import {UrlUtils} from "../../services/url-utils";
+import {CategoryIncomeType} from "../../../types/category-income.type";
+import {ExpenseService} from "../../services/expense-service";
 
+//done
 export class IncomeEdit {
+    readonly titleInputElement: HTMLInputElement | null;
+    private incomeOriginalData: CategoryIncomeType | null;
+
     constructor() {
-        document.getElementById('updateButton').addEventListener('click', this.updateIncome.bind(this));
+        const updateButton: HTMLElement | null = document.getElementById('updateButton');
+        if (updateButton) {
+            updateButton.addEventListener('click', this.updateIncome.bind(this));
+        }
 
-        this.titleInputElement = document.getElementById('titleInput');
+        this.titleInputElement = document.getElementById('titleInput') as HTMLInputElement;
 
-        const id = UrlUtils.getUrlParam('id');
+        const id: string | null = UrlUtils.getUrlParam('id');
         if (!id) {
             window.location.href = '/#';
         }
@@ -15,47 +24,63 @@ export class IncomeEdit {
         this.getIncome(id).then();
     }
 
-    validateForm() {
-        let isValid = true;
-        if (this.titleInputElement.value) {
-            this.titleInputElement.classList.remove('is-invalid');
-        } else {
-            this.titleInputElement.classList.add('is-invalid');
-            isValid = false;
+    private validateForm(): boolean {
+        let isValid: boolean = true;
+        if (this.titleInputElement) {
+            if (this.titleInputElement.value) {
+                this.titleInputElement.classList.remove('is-invalid');
+            } else {
+                this.titleInputElement.classList.add('is-invalid');
+                isValid = false;
+            }
         }
         return isValid;
     }
 
-    async getIncome(id) {
-        const response = await IncomeService.getIncome(id);
+    private async getIncome(id: number): Promise<void> {
+        const response: CategoryIncomeType = await IncomeService.getIncome(id);
 
         if (!response) {
             alert("Произошла ошибка");
             window.location.href = '/#';
+            return;
         }
 
         this.incomeOriginalData = response;
-        this.titleInputElement.value = response.title;
+        if (this.titleInputElement) {
+            this.titleInputElement.value = response.title;
+        }
     }
 
-    async updateIncome(e) {
+    private async updateIncome(e: MouseEvent): Promise<void> {
         e.preventDefault();
 
         if (this.validateForm()) {
-            const changedData = {};
+            const changedData: CategoryIncomeType = {title: ''};
+            if (!this.titleInputElement || !this.incomeOriginalData) {
+                window.location.href = '#/expense';
+                return;
+            }
+
             if (this.titleInputElement.value !== this.incomeOriginalData.title) {
                 changedData.title = this.titleInputElement.value;
             }
             if (Object.keys(changedData).length > 0) {
-                const response = await IncomeService.updateIncome(this.incomeOriginalData.id, changedData);
+                const incomeId: number | undefined = this.incomeOriginalData.id;
+                if (incomeId !== undefined) {
+                    const response: CategoryIncomeType = await ExpenseService.updateExpense(incomeId, changedData);
 
-                if (!response) {
+                    if (!response) {
+                        alert("Произошла ошибка");
+                    }
+
+                    window.location.href = '#/income';
+                    return;
+                } else {
                     alert("Произошла ошибка");
                 }
-
-                return window.location.href = '#/income';
             }
         }
-        return window.location.href = '#/income';
+        window.location.href = '#/income';
     }
 }
