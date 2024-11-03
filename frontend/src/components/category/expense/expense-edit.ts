deff4cd08a72931978276d627c6ca6b8b657205d
Polina Muravieva <polinamur3@gmail.com>
2024-11-03 23:30:41 +0300

    expense-edit.js file converted to ts

--- frontend/src/components/category/expense/expense-edit.ts
+++ frontend/src/components/category/expense/expense-edit.ts
@@ -1,61 +1,86 @@
 import {UrlUtils} from "../../services/url-utils";
 import {ExpenseService} from "../../services/expense-service";
+import {CategoryExpenseType} from "../../../types/category-expense.type";
 
+//done
 export class ExpenseEdit {
+    readonly titleInputElement: HTMLInputElement | null;
+    private expenseOriginalData: CategoryExpenseType | null;
+
     constructor() {
-        document.getElementById('updateButton').addEventListener('click', this.updateExpense.bind(this));
+        const updateButton: HTMLElement | null = document.getElementById('updateButton');
+        if (updateButton) {
+            updateButton.addEventListener('click', this.updateExpense.bind(this));
+        }
 
-        this.titleInputElement = document.getElementById('titleInput');
+        this.titleInputElement = document.getElementById('titleInput') as HTMLInputElement;
 
-        const id = UrlUtils.getUrlParam('id');
+        const id: string | null = UrlUtils.getUrlParam('id');
         if (!id) {
             window.location.href = '/#';
+            return;
         }
 
         this.getExpense(id).then();
     }
 
-    validateForm() {
-        let isValid = true;
-        if (this.titleInputElement.value) {
-            this.titleInputElement.classList.remove('is-invalid');
-        } else {
-            this.titleInputElement.classList.add('is-invalid');
-            isValid = false;
+    private validateForm(): boolean {
+        let isValid: boolean = true;
+        if (this.titleInputElement) {
+            if (this.titleInputElement.value) {
+                this.titleInputElement.classList.remove('is-invalid');
+            } else {
+                this.titleInputElement.classList.add('is-invalid');
+                isValid = false;
+            }
         }
         return isValid;
     }
 
-    async getExpense(id) {
-        const response = await ExpenseService.getExpense(id);
+    private async getExpense(id): Promise<void> {
+        const response: CategoryExpenseType = await ExpenseService.getExpense(id);
 
         if (!response) {
             alert("Произошла ошибка");
             window.location.href = '/#';
+            return;
         }
 
         this.expenseOriginalData = response;
-        this.titleInputElement.value = response.title;
+        if (this.titleInputElement) {
+            this.titleInputElement.value = response.title;
+        }
     }
 
-    async updateExpense(e) {
+    private async updateExpense(e): Promise<void> {
         e.preventDefault();
 
         if (this.validateForm()) {
-            const changedData = {};
-            if (this.titleInputElement.value !== this.expenseOriginalData.title) {
+            const changedData: CategoryExpenseType = {title: ''};
+            if (!this.titleInputElement || !this.expenseOriginalData) {
+                window.location.href = '#/expense';
+                return;
+            }
+
+            if (this.titleInputElement.value !== (this.expenseOriginalData as CategoryExpenseType).title) {
                 changedData.title = this.titleInputElement.value;
             }
             if (Object.keys(changedData).length > 0) {
-                const response = await ExpenseService.updateExpense(this.expenseOriginalData.id, changedData);
+                const expenseId: number | undefined = this.expenseOriginalData.id;
+                if (expenseId !== undefined) {
+                    const response: CategoryExpenseType = await ExpenseService.updateExpense(expenseId, changedData);
 
-                if (!response) {
+                    if (!response) {
+                        alert("Произошла ошибка");
+                    }
+
+                    window.location.href = '#/expense';
+                    return;
+                } else {
                     alert("Произошла ошибка");
                 }
-
-                return window.location.href = '#/expense';
             }
         }
-        return window.location.href = '#/expense';
+        window.location.href = '#/expense';
     }
 }
