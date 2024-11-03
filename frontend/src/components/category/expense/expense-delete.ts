e8a5deb1dd880c263f081d84349453ab79056774
Polina Muravieva <polinamur3@gmail.com>
2024-11-03 23:11:23 +0300

    expense-delete.js file converted to ts

--- frontend/src/components/category/expense/expense-delete.ts
+++ frontend/src/components/category/expense/expense-delete.ts
@@ -1,25 +1,27 @@
 import {UrlUtils} from "../../services/url-utils";
 import {ExpenseService} from "../../services/expense-service";
 
+//done
 export class ExpenseDelete {
     constructor() {
-        const id = UrlUtils.getUrlParam('id');
+        const id: string | null = UrlUtils.getUrlParam('id');
         if (!id) {
             window.location.href = '/#';
+            return;
         }
 
         this.deleteExpense(id).then();
     }
 
-
-    async deleteExpense(id) {
-        const response = await ExpenseService.deleteExpense(id);
+    private async deleteExpense(id): Promise<void> {
+        const response: boolean = await ExpenseService.deleteExpense(id);
 
         if (!response) {
             alert("Произошла ошибка");
-            return window.location.href = '#/';
+            window.location.href = '#/';
+            return;
         }
 
-        return window.location.href = '#/expense';
+        window.location.href = '#/expense';
     }
 }
